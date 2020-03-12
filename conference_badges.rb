487831769e60d0bdc80fead5260a656997fe344d
Philippe Timothe <philippetimothe@gmail.com>
2020-03-12 16:51:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,9 +15,7 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  attendees.each {|attendee| 
-
-    
-  }
+  def batch_badge_creator.each {|attendees| attendees}
+  def assign_rooms.each {|attendees| attendees}  
   
 end
