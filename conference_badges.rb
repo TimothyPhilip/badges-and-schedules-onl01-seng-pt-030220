4571f80e6d633ece87ebe7b9866e88f639880856
Philippe Timothe <philippetimothe@gmail.com>
2020-03-12 15:32:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badge_messages = []
+  names.each {|name| badge_messages << "Hello, my name is #{name}."}
+  badge_messages
+end  
+
+def assign_rooms(attendees)
+  room_assignments = []
+  attendees.each_with_index {|attendee, index| room_assignments << "Hello, #{attendee}! You'll be assigned to room #{index+1}!" }
+  room_assignments
+end
+
+def printer(attendees)
+  badge_messages.each {||
+
+end
